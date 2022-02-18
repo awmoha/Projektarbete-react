2d786d7fa6918969b1e9be6b5c7611650bdde8fb
Mohammad Awad <mohammad.awad0110@gmail.com>
2022-02-18 09:12:54 +0100

    added Link and Router amd Switch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
 import style from "./style.css"
+import { Switch, Route } from 'react-router-dom';
+
 function App() {
   return (
     <div className="App">
+      <header className='App-header'>
+        <Switch>
+          
+        </Switch>
+      </header>
       
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import store from "./redux/configureStore";
+import { BrowserRouter as Router } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
+      <Router>
       <App />
+      </Router>
     </Provider>
   </React.StrictMode>,
   document.getElementById("root")
