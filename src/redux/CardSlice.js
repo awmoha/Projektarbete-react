eb4b3e35a28fe9a6d4546e500af6847ceda6af2a
Mohammad Awad <mohammad.awad0110@gmail.com>
2022-02-18 17:32:44 +0100

    added fetchRandomUsers api i CardSlice

--- src/redux/CardSlice.js
+++ src/redux/CardSlice.js
@@ -1,29 +1,38 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+
+export const fetchRandomUser = createAsyncThunk(
+  "card/fetchRandomUser",
+  async () => {
+    return fetch(`https://randomuser.me/api/`)
+      .then((response) => response.json())
+      .then((data) => data.results[0]);
+  }
+);
 const CardSlice = createSlice({
   name: "card",
   initialState: {
     infoCard: [
       {
         id: 1,
-        name: "Moha",
-        endor: "Master",
-        cardNumber: "1111 1111 1111 1111",
-        cardMonth: "11",
-        cardYear: "11",
-        bankName: "Visa",
+        name: "",
+        endor: "",
+        cardNumber: "",
+        cardMonth: "",
+        cardYear: "",
+        bankName: "",
         cardsStateActiv: false,
       },
     ],
-    
+
     lateId: 1,
   },
-  
 
-    reducers: {
-      addNewCard: (state, action) => {
-        state.infoCard.push(action.payload);
-        state.lateId +=1;
-      },
+  reducers: {
+    addNewCard: (state, action) => {
+      state.infoCard.push(action.payload);
+      state.lateId += 1;
+    },
+    extraRedusers:{}
   },
 });
 export const { addNewCard } = CardSlice.actions;
