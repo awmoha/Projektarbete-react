00aacc1ced05eee414c98d54a3c68e973f7a64ef
Mohammad Awad <mohammad.awad0110@gmail.com>
2022-02-18 11:45:28 +0100

    added style for card

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -4,7 +4,7 @@ function Card({card}) {
   return (
     <div>
         <h2>Info Card</h2>
-        <div >
+        <div className='card_div'>
             <p>{card.name}</p>
             <p>{card.endor}</p>
             <p>{card.cardNumber}</p>
