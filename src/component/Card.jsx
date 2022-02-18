86d16e93917ae704e63e653e99e88411448505e4
Mohammad Awad <mohammad.awad0110@gmail.com>
2022-02-18 11:36:10 +0100

    added cardsInfo from CardSlice

--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
 
-function Card() {
+function Card({card}) {
   return (
-    <div>Card</div>
+    <div>
+        <h2>Info Card</h2>
+        <div >
+            <p>{card.name}</p>
+            <p>{card.endor}</p>
+            <p>{card.cardNumber}</p>
+            <p>{card.cardMonth}</p>
+            <p>{card.cardYear}</p>
+            <p>{card.bankName}</p>
+
+        </div>
+    </div>
   )
 }
 
