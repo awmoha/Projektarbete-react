ceec67ac11bb1baa7bd04692f18f302bf32f27ea
Mohammad Awad <mohammad.awad0110@gmail.com>
2022-02-18 09:02:21 +0100

    added.component and pages

---
+++ src/component/AddCards.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function AddCards() {
+  return (
+    <div>Add.card</div>
+  )
+}
+
+export default AddCards
---
+++ src/pages/AddCard.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const AddCard = (props) => {
+  return (
+    <div>AddCard</div>
+  )
+}
+
+export default AddCard
