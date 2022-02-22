f92bf00a28da7e06a1487cc6a7368e04202a887c
Mohammad Awad <mohammad.awad0110@gmail.com>
2022-02-22 13:07:16 +0100

    have two ide, create a new array or work on one array

--- src/component/Cardinfo.jsx
+++ src/component/Cardinfo.jsx
@@ -1,16 +1,17 @@
 import Card from "./Card";
 import { useSelector } from "react-redux";
 import { useState } from "react";
-
+import NotActiveCard from "./NotActiveCard";
 const CardInfo = () => {
   const { infoCard } = useSelector((state) => state.cardInfo);
+  //försöka att loopa igenom array och kolla om det funkar med activ 
+  const { notActiveCards } = useSelector((state) => state.cardInfo);
   const [isActive, setisActive] = useState();
   let handleActiv = () => {
     for (let i = 0; i < infoCard.length; i++)
       if (infoCard[1] === infoCard[0]) {
         infoCard.usshift([0]);
       }
-
     console.log(infoCard[1]);
     // console.log(activeCardIndex);
     // console.log(infoCard[1].isActive)
@@ -25,11 +26,11 @@ const CardInfo = () => {
       </div>
 
       {/* <div>
-        <p> Cards</p>
-        {cardList.map((creditList, i) => (
+        <p> InActive Cards</p>
+        {notActiveCards.map((creditList, i) => (
           <NotActiveCard key={i} creditList={creditList} />
         ))}
-      </div> */}
+      </div>  */}
     </div>
   );
 };
